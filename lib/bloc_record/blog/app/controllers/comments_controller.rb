8a13af98443ed4026671b10a0ef70bcbdb37fba1
couentine <quent.guillaume@gmail.com>
2019-02-01 09:29:18 -0800

    correction

--- lib/bloc_record/blog/app/controllers/comments_controller.rb
+++
@@ -1,8 +0,0 @@
-class CommentsController < ApplicationController
-
-def create
-  @article = Article.find(params[:article_id])
-  @comment = @article.comments.create!(params[:comment].permit!)
-  redirect_to @article, :notice => "Comment created!"
-end
-end
--- lib/bloc_record/blog/app/models/author.rb
+++
@@ -1,7 +0,0 @@
-class Author
-  include Mongoid::Document
-  field :name
-  field :_id, type: String, default: ->{ name }
-  embeds_many :articles
-  
-end
