47b767624bd063dae610245dffea4c15611af69b
couentine <quent.guillaume@gmail.com>
2019-02-06 13:24:01 -0800

    6-assign first

--- lib/bloc_record/collection.rb
+++ lib/bloc_record/collection.rb
@@ -6,4 +6,10 @@ module BlocRecord
 			self.any? ? self.first.class.update(ids, updates) : false
 		end
 	end
+
+	 def destroy_all
+      ids = self.map(&:id)
+     self.any? ? self.first.class.destroy(ids) : false
+    end	   
 end
+
